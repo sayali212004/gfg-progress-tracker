2f4fd946730c4bd22e4c81471aeaa654bd076600
Sayali Shete <shetesayali2004@gmail.com>
2025-07-21 22:48:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print GeeksForGeeks/print-geeks-for-geeks.java
@@ -0,0 +1,18 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Prints Hello World
+        // System.out.println("Hello World");
+        // Prints GeeksForGeeks
+        System.out.println("GeeksForGeeks");
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
