c7ffe390f9e0ae1a6d68153e383ef88276e6e339
Sayali Shete <shetesayali2004@gmail.com>
2025-07-21 22:38:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input - Java/taking-input-java.java
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        // Take all inputs
+        // Print all the values
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        float b=sc.nextFloat();
+        long c=sc.nextLong();
+        byte d=sc.nextByte();
+        sc.nextLine();
+        String e=sc.nextLine();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
