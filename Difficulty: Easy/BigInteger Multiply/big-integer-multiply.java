caf2ad3c4462de3a2b9378f58e456f3cb0315a6f
Sayali Shete <shetesayali2004@gmail.com>
2025-07-21 22:38:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BigInteger Multiply/big-integer-multiply.java
@@ -0,0 +1,13 @@
+/* Class MathematicalOperation with add function
+ * a, b : Two parameters of BigIntegers to multiply
+ */
+import java.math.BigInteger;
+
+class MathematicalOperation {
+
+    // multiply function that returns the multiplication of a and b
+    static BigInteger mul(BigInteger a, BigInteger b) {
+        // Your code here
+        return a.multiply(b);
+    }
+}
