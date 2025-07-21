c0d3244c4c1a0eaf86f40a1fc657521c8a74efe0
Sayali Shete <shetesayali2004@gmail.com>
2025-07-21 22:50:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pass by Reference and Value/pass-by-reference-and-value.java
@@ -0,0 +1,13 @@
+
+
+// User function Template for Java
+
+class Solution {
+    static int[] passedBy(int a, int b) {
+        // code here
+        a=a+1;
+        b=b+2;
+        return new int []{a,b};
+        
+    }
+}
