411053c3b7888ef22e90910ac1f6751bcf06f2a2
Sayali Shete <shetesayali2004@gmail.com>
2025-07-21 22:48:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Constructor Overloading/constructor-overloading.java
@@ -0,0 +1,26 @@
+
+//User function Template for Java
+
+// design a class with two constructor
+// one default, the other parameterized
+
+// code here
+class User
+{
+    String name;
+    
+    User()
+    {
+        this.name="Default";
+    }
+    
+    User(String name)
+    {
+        this.name=name;
+        
+    }
+}
+
+
+
+
