13ec322c2fb9f903e3445ccb54a0efac2f08ff16
Sayali Shete <shetesayali2004@gmail.com>
2025-07-21 22:49:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Alphabets/print-alphabets.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+
+// function should print the alphabets
+// from c1 to c2 (space separated)
+// No need to print the new line
+// The new line will be printed by the
+// driver's code.
+class Solution {
+    public static void alphabets(char c1, char c2) {
+        // code here
+        while(c1<=c2)
+        {
+            System.out.print(c1+" ");
+            c1++;
+        }
+        
+    }
+}
