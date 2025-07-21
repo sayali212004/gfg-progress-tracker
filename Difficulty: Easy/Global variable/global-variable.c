931d3bb0eb407215ce8fca0a446f6ec0b708b956
Sayali Shete <shetesayali2004@gmail.com>
2025-07-21 22:47:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Global variable/global-variable.c
@@ -0,0 +1,13 @@
+//Back-end complete function Template for C
+
+#include <stdio.h>
+    int g=10;
+
+// declare a global variable g
+
+int main() {
+    // printing the value of global variable g
+    printf("%d", g);
+
+    return 0;
+}
