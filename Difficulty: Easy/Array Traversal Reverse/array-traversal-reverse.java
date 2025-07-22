901ca82e70c821e9f2ebea521a00160fa623fd32
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 08:22:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Traversal Reverse/array-traversal-reverse.java
@@ -0,0 +1,13 @@
+
+// User function Template for Java
+class Solution{
+public static void arrayTraversalReverse(int[] arr) {
+    // Write your code below
+
+    // Don't add a new line as it is provided by the driver code
+    for(int i=arr.length-1;i>=0;i--)
+    {
+        System.out.print(arr[i]+" ");
+    }
+}
+}
