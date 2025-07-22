d5139e90fe963aa7c832549c533a4f00cc2b05ef
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 09:44:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Without Newline/print-without-newline.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void utility(Scanner sc) {
+        // code here
+        // Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        // Print in the required format without new Line
+        System.out.print(a+" "+b);
+    }
+}
