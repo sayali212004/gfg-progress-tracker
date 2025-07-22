b5afe3d0bb2a3d9eb8759ee5e172457ab9f51596
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 08:34:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Decrement Array Values/decrement-array-values.java
@@ -0,0 +1,13 @@
+
+// User function Template for Java
+class Solution{
+public static int[] decrementArrayElements(int[] arr) {
+
+    // Write your code here
+    for(int i=0;i<arr.length;i++)
+    {
+        arr[i]=arr[i]-1;
+    }
+    return arr;
+}
+}
