de5a1716477ef310a72e5e4144f4331a65f5ab51
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 08:20:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Addition/addition.java
@@ -0,0 +1,16 @@
+
+
+// create a class addition
+// with a static function add
+// this should return the sum of
+// two values(a,b) passed in the parameter
+
+// code here
+class Addition
+{
+    public static int add(int a, int b)
+    {
+        int sum=a+b;
+        return sum;
+    }
+}
