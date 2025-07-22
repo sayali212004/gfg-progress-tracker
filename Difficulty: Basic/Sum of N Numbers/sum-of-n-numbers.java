6418e7eccc2e764cea33b13ebb45431e5639578c
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 10:00:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int nSum(int n) {
+        // code here
+        int ans = 0;
+
+    //Write your code here to calculate and return sum of n number.
+    ans=(n*(n+1))/2;
+    
+    
+    
+    return ans;
+    }
+}
