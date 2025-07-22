7724502c6453e9762975a9a4acdd0ac81d4e566a
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 09:51:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Bitwise Right Shift/bitwise-right-shift.java
@@ -0,0 +1,14 @@
+
+// User function Template for Java
+class Solution
+{
+public static void rightShift(int a, int b) {
+
+    // just complete below statement
+    int ans =a>>b;
+
+        // print the result
+        System.out.println(ans);
+}
+
+}
