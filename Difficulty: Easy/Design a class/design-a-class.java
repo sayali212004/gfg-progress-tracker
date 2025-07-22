0728314faa3916658b7179f7511863ae6ef31fe7
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 08:19:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Design a class/design-a-class.java
@@ -0,0 +1,13 @@
+
+//User function Template for Java
+
+// Design a class as described in the question
+// Code here
+class MyClass
+{
+    public void display()
+    {
+        System.out.println("Hello World");
+    }
+}
+
