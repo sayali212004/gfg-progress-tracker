a714cdd5cb6b31b17f7e4268444e8bdd2af58217
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 09:49:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Square wall/print-square-wall.java
@@ -0,0 +1,16 @@
+class Solution
+{
+// User function Template for Java
+public static void squareWall(int s) {
+    // Write your code here
+    for(int i=0;i<s;i++)
+    {
+        for(int j=0;j<s;j++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
+
+}
