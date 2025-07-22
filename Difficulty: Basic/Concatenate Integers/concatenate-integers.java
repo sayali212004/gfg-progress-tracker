9959a32b5f5f6090141d34b3ca807e8b5bb4372c
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 09:54:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Concatenate Integers/concatenate-integers.java
@@ -0,0 +1,21 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Write Your Code to Concatenate and print
+        int res=Integer.parseInt(Integer.toString(a)+Integer.toString(b));
+        System.out.println(res);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
