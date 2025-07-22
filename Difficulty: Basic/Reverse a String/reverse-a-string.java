8b5046bd5b3742be55018bf1e6f8eae0521eb79b
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 09:55:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.java
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -1,23 +1,21 @@
-// User function Template for Java
-
 class Solution {
-    public static String reverseString(String s) {
+    static String revStr(String s) {
         // code here
-        char [] arr=s.toCharArray();
-        int n = arr.length;
+        char arr[]=s.toCharArray();
+        
+        int n=arr.length;
         int left=0;
         int right=n-1;
-        for(int i=0;i<n;i++)
+        
+        while(left<right)
         {
-            while(left<right)
-            {
-                char temp=arr[left];
-                arr[left]=arr[right];
-                arr[right]=temp;
-                left++;
-                right--;
-            }
+            char temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
         }
+        
         return new String(arr);
     }
 }
