cc0b4b6c6825c4ccfa2e0fee86f101fd7a141619
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 08:13:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of Big Integers/sum-of-big-integers.java
@@ -0,0 +1,14 @@
+/* Class Solution with add fucntion
+ * a, b : Two parameters of BigIntegers to add
+ */
+import java.math.BigInteger;
+
+class Solution {
+
+    static BigInteger add(BigInteger a, BigInteger b) {
+
+        // Your code here
+        
+        return a.add(b);
+    }
+}
