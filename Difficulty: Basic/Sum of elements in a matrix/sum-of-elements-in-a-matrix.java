07b7dc83094ca1a821f6acaa99186f9b8dc7b951
Sayali Shete <shetesayali2004@gmail.com>
2025-07-22 09:35:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of elements in a matrix/sum-of-elements-in-a-matrix.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+
+class Solution {
+    int sumOfMatrix(int N, int M, int[][] Grid) {
+        // code here
+        int ans=0;
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<M;j++)
+            {
+                ans=ans+Grid[i][j];
+            }
+        }
+        return ans;
+    }
+}
