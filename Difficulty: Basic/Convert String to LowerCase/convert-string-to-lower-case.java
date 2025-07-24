e5bd1d7566f8f12e9ea94e1aa89e4c1024fd26d7
Sayali Shete <180266581+sayalii2004@users.noreply.github.com>
2025-07-24 12:45:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Convert String to LowerCase/convert-string-to-lower-case.java
@@ -0,0 +1,9 @@
+// User function template for Java
+
+class Solution {
+    static String toLower(String s) {
+        // code here
+        String lower=s.toLowerCase();
+        return lower;
+    }
+}
