007b9226348ae33cbf5285ad4d834eaf91f17c5a
Sayali Shete <180266581+sayalii2004@users.noreply.github.com>
2025-07-29 16:58:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Doubling the value/doubling-the-value.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Solution {
+    long solve(int n, long A[], long b) {
+        // Complete this function
+        for (int i = 0; i < n; i++) {
+            if (A[i] == b) {
+                b = b * 2;
+            }
+        }
+        return b;
+    }
+}
